575355a5d8980d9e9d38d1e369e4c8ddf48b91f0
Bob Myers <rtm@gol.com>
2014-09-26 17:37:52 +0530

    tabify/format

--- app.js
+++ app.js
@@ -4,40 +4,41 @@ import {createUpwardableObject, DIV, TEXT, BUTTON, INPUT} from 'upward';
 import {makeStyles} from 'style';
 
 var obj = createUpwardableObject({ 
-	textnode: document.createTextNode("Hello, world."),
-	string: 'fuck',
-	buttonText: 'Press me',
+  textnode: document.createTextNode("Hello, world."),
+  string: 'fuck',
+  buttonText: 'Press me',
 });
 
 
 var button = BUTTON().child(TEXT(obj.buttonText));
 var change = function() {
-	obj.textnode=document.createTextNode("Hello, upward world.");
-	obj.string = "shit";
+  obj.textnode=document.createTextNode("Hello, upward world.");
+  obj.string = "shit";
 };
 button.addEventListener('click', change);
 
 var input = INPUT();
 
 var app = {
-	root: 'app',
-	DOM: DIV()
-//		.child(obj.textnode)
-		.child(button)
-		.child(TEXT(obj.string))
-		.child(input)
-		.child(TEXT(input.val))
-	,
-	CSS: [
-		['body', {backgroundColor: 'cyan'}]
-	],
-	listeners: {}
+  root: 'app',
+  DOM: DIV()
+  //    .child(obj.textnode)
+    .child(button)
+    .child(TEXT(obj.string))
+    .child(input)
+    .child(TEXT(input.val_input))
+
+  ,
+  CSS: [
+    ['body', {backgroundColor: 'cyan'}]
+  ],
+  listeners: {}
 };
 
 var Run = function(app) {
-	var root = document.getElementById(app.root);
-	root.appendChild(app.DOM);
-	makeStyles(root, app.CSS);
+  var root = document.getElementById(app.root);
+  root.appendChild(app.DOM);
+  makeStyles(root, app.CSS);
 };
 
 Run(app);
--- upward.js
+++ upward.js
@@ -8,46 +8,46 @@ var valueOf = v => v.valueOf();
 
 function _this() { return this; }
 
-Object.prototype.map = function (fn) {
-	return keys(this).reduce((result, k) => (result[k] = fn(this[k], k, this), result), {});
-};
+//Object.prototype.map = function (fn) {
+//  return keys(this).reduce((result, k) => (result[k] = fn(this[k], k, this), result), {});
+//};
 
-//Object.prototype.valueOf = function() {	return this.map(valueOf); };
+//Object.prototype.valueOf = function() { return this.map(valueOf); };
 //RegExp.prototype.valueOf = Date.prototype.valueOf = _this;
 
 var chainify = fn => function() { fn.apply(this, arguments); return this; };
 
 export function Upwardable(v) {
-	if (isUpwardable(v)) { return v; }
+  if (isUpwardable(v)) { return v; }
 
-	var upwards       = [];
+  var upwards       = [];
 
-	var valueOf       = () => v === null || v === undefined ? v : v.valueOf();
-	var set           = nv => { send_upwards(nv); v = nv; return this; };
-	var get           = () => ret;
+  var valueOf       = () => v === null || v === undefined ? v : v.valueOf();
+  var set           = nv => { send_upwards(nv); v = nv; return this; };
+  var get           = () => ret;
 
-	var upward        = upward => { upwards.push(upward); };
-	var ununpward     = function() { return this; };
-	var send_upwards  = nv => upwards.forEach(fn => fn(nv.valueOf(), v.valueOf()));
-
-	var ret           = {get, set, valueOf, upward};
- 
-    var onward        = function(f, ...args) {
-		function go() {	return f.apply(this.valueOf(), ...args); }
-		upward(go);
-		return go();
-	};
-	
-	return ret;
-};
+  var upward        = upward => { upwards.push(upward); };
+  var ununpward     = function() { return this; };
+  var send_upwards  = nv => upwards.forEach(fn => fn(nv.valueOf(), v.valueOf()));
+  
+  var ret           = {get, set, valueOf, upward};
+  
+  var onward        = function(f, ...args) {
+    function go() { return f.apply(this.valueOf(), ...args); }
+    upward(go);
+    return go();
+  };
+  
+  return ret;
+}
 
 function upwardablePropertyDescriptor(val) {
-	var {get, set} = Upwardable(val);
-	return { get, set, enumerable: true };
+  var {get, set} = Upwardable(val);
+  return { get, set, enumerable: true };
 }
 
 function defineUpwardableProperty(obj, prop, val) {
-	return isUpwardable(val) ? val : defineProperty(obj, prop, upwardablePropertyDescriptor(val));
+  return isUpwardable(val) ? val : defineProperty(obj, prop, upwardablePropertyDescriptor(val));
 }
 
 export var isUpwardable = x => x && typeof x === 'object' && x.upward;
@@ -56,69 +56,73 @@ var upward = (o, fn) => o.upward && o.upward(fn);
 
 
 export function upwardify(fn, changefn = fn) {
-	return function(v) {
-		upward(v, changefn.bind(this));
-		return fn.call(this, v.valueOf());
-	};
+  return function(v) {
+    upward(v, changefn.bind(this));
+    return fn.call(this, v.valueOf());
+  };
 }
 
 export function createUpwardableObject(o) {
-	return defineProperties({}, keys(o).reduce((result, k) => {
-		result[k] = upwardablePropertyDescriptor(o[k]);
-		return result;
-	}, {}));
+  return defineProperties({}, keys(o).reduce((result, k) => {
+    result[k] = upwardablePropertyDescriptor(o[k]);
+    return result;
+  }, {}));
 }
 
 /** testing
-function upwardArray(a) {
-	var w = Upwardable(a);
-	a.forEach(elt => upward(elt, () => w.set(Array(a))));
-	return w;
-}
+    function upwardArray(a) {
+    var w = Upwardable(a);
+    a.forEach(elt => upward(elt, () => w.set(Array(a))));
+    return w;
+    }
 */
 
 var joiner = delimiter => function() { return join.call(this, delimiter); };
 
 export var upwardableTextNode = upwardify(
-	createTextNode.bind(document),
-	function(text) {this.nodeValue = text;}
+  createTextNode.bind(document),
+  function(text) {this.nodeValue = text;}
 );
 
 var {appendChild, replaceChild, setAttribute} = HTMLElement.prototype;
 
 Object.assign(HTMLElement.prototype, {
-	child: upwardify(chainify(appendChild), replaceChild),
-	attr: upwardify(chainify(setAttribute), setAttribute)
+  child: upwardify(chainify(appendChild), replaceChild),
+  attr: upwardify(chainify(setAttribute), setAttribute)
 });
 
 Object.assign(Node.prototype, {
-    value: upwardify(chainify(function(v) { this.nodeValue = v || ""; })),
-	toValue: _this
+  value: upwardify(chainify(function(v) { this.nodeValue = v || ""; })),
+  toValue: _this
 });
 
 CSSStyleSheet.prototype.replaceRule = function(rule, idx) {
-	this.deleteRule(idx);
-	return this.insertRule(rule, idx);
+  this.deleteRule(idx);
+  return this.insertRule(rule, idx);
 };
 
 export var INPUT = function() {
-	var input = document.createElement('input');
-	defineUpwardableProperty(input, 'val', "");
-	input.addEventListener('change', function() { input.val = input.value; });
-	return input;
+  var input = document.createElement('input');
+  var propname = evt_type => `val_${evt_type}`;
+  var handler = { handleEvent: evt => input[propname(evt.type)] = input.value };
+  ['input', 'change'].forEach(evt_type => {
+    input.addEventListener(evt_type, handler);
+    defineUpwardableProperty(input, propname(evt_type), "");
+  });
+  return input;
 };
 
 export var BUTTON = function() {
-	return document.createElement('button');
+  return document.createElement('button');
 };
 
 Node.prototype.toValue = _this;
 
 export var DIV = function() {
- 	return document.createElement('div');
- };
+  return document.createElement('div');
+};
 
 export var TEXT = function(text) {
-	return document.createTextNode("").value(text);
+  return document.createTextNode("").value(text);
 };
 
