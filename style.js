a1f387d5406a63feb96e4f25ea5b7bf2a5edf70a
Bob Myers <rtm@gol.com>
2014-09-27 23:10:05 +0530

    Re-indent style.js.

--- style.js
+++ style.js
@@ -1,27 +1,27 @@
 /* jshint esnext: true */
 
 export function makeStyles(elt, css) {
-	var style = document.createElement('style');
-    var sheet;
+  var style = document.createElement('style');
+  var sheet;
 
-	style.appendChild(document.createTextNode(""));
-	style.setAttribute('scoped', true);
-	elt.appendChild(style);
-	sheet = style.sheet;
-	
-	(css || []).forEach(function(rule) {
-		Object.assign(
-			sheet.cssRules[
-				sheet.insertRule(
-					[].concat(rule[0]).join(',') + '{}', 
-					sheet.cssRules.length)
-			].style,
-			...[].concat(rule[1])
-		);;
-	});
+  style.appendChild(document.createTextNode(""));
+  style.setAttribute('scoped', true);
+  elt.appendChild(style);
+  sheet = style.sheet;
+  
+  (css || []).forEach(function(rule) {
+    Object.assign(
+      sheet.cssRules[
+        sheet.insertRule(
+          [].concat(rule[0]).join(',') + '{}', 
+          sheet.cssRules.length)
+      ].style,
+				...[].concat(rule[1])
+    );;
+  });
 }
 
 
-	
-	
+  
+  
 
