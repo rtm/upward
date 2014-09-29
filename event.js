228910e347608fceb7e41abb9e21258e43afeee8
Bob Myers <rtm@gol.com>
2014-09-29 10:11:01 +0530

    Document event.js a little bit.

--- event.js
+++ event.js
@@ -1,11 +1,30 @@
+// Event handling
+// ==============
+
+// Events are handled by calling `on` on an element, and passing a hash of handlers.
+//
+// Example:
+// ```
+// BUTTON().on({click: handleButtonClick});
+//
+// function handleButtonClick(evt) {
+//     // this.context is the button
+// }
+// ```
+
+// Define a prototypical `handleEvent` on `EventListener`s.
+// This dispatches events to a method of the same name.
+// Event-handling methods can examine `this.context` to get the underlying `this`.
 var EventListenerPrototype = {
   handleEvent(evt) { this[evt.type](evt); }
 };
 
 
+// `on` is placed on `EventTargets` (meaning HTML elements), 
+// taking a hash of handlers keyed by event name.
 EventTarget.prototype.on = function(handlers) {
   var listener = Object.create(EventListenerPrototype);
-  Object.assign(listener, handlers, {target: this});
+  Object.assign(listener, handlers, {context: this});
 
   Object.keys(handlers).forEach(function(evt_name) {
     this.addEventListener(evt_name, listener);
