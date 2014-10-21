a9becd26913c823f23051ad0d09d73f87aa0da17
Bob Myers <rtm@gol.com>
2014-10-21 22:48:57 +0530

    Clean up files

--- U.js
+++
@@ -1,9 +0,0 @@
-export {
-	Upwardable as               U,
-  computedUpwardable as       C,
-  upwardifyProperties as      P,
-  valueOf as                  V,
-  upwardifyTemplate as        S,
-  upwardifyTemplateFormula as S$,
-	createElt as                E
-} from './upward';
