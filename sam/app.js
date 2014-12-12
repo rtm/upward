10ede7d59ebd7c70ad7da3381be0227609f91e80
Bob Myers <rtm@gol.com>
2014-12-12 10:59:40 +0530

    Random f**king around with MVC shit.

---
+++ sam/app.js
@@ -0,0 +1,14 @@
+import {makeView} from '../Mvc/Mvc';
+import {U} from '../src/Up';
+
+var model = U({name: 'Upward - Todos'});
+
+function controller() { }
+
+function view(model, controller) {
+  return E('div');
+}
+
+export default function makeAppView(model) {
+  return makeView(model, view, controller);
+}
